e2b8b0772dd04f41ef17c1a4fed34c1a3a659d24
Giddy-K <gideonkipamet@gmail.com>
2025-04-23 23:43:35 +0300

    Added Fallbacks first for small screens

--- src/styles.js
+++ src/styles.js
@@ -1,17 +1,17 @@
 const styles = {
-    paddingX: "sm:px-16 px-6",
-    paddingY: "sm:py-16 py-6",
-    padding: "sm:px-16 px-6 sm:py-16 py-10",
-  
-    heroHeadText:
-      "font-black text-white lg:text-[80px] sm:text-[60px] xs:text-[50px] text-[40px] lg:leading-[98px] mt-2",
-    heroSubText:
-      "text-[#dfd9ff] font-medium lg:text-[30px] sm:text-[26px] xs:text-[20px] text-[16px] lg:leading-[40px]",
-  
-    sectionHeadText:
-      "text-white font-black md:text-[60px] sm:text-[50px] xs:text-[40px] text-[30px]",
-    sectionSubText:
-      "sm:text-[18px] text-[14px] text-secondary uppercase tracking-wider",
-  };
-  
-  export { styles };
+  paddingX: "px-6 sm:px-16",
+  paddingY: "py-6 sm:py-16",
+  padding: "px-6 py-10 sm:px-16 sm:py-16",
+
+  heroHeadText:
+    "font-black text-white text-[30px] xs:text-[40px] sm:text-[60px] lg:text-[80px] lg:leading-[98px] mt-2",
+  heroSubText:
+    "text-[#dfd9ff] font-medium text-[14px] xs:text-[16px] sm:text-[26px] lg:text-[30px] lg:leading-[40px]",
+
+  sectionHeadText:
+    "text-white font-black text-[24px] xs:text-[30px] sm:text-[50px] md:text-[60px]",
+  sectionSubText:
+    "text-[12px] xs:text-[14px] sm:text-[18px] text-secondary uppercase tracking-wider",
+};
+
+export { styles };
