23da27f1e4f2692e96719a70abf93cb3e3b7af84
Giddy-K <gideonkipamet@gmail.com>
2025-04-24 01:59:34 +0300

    Minor adjustments

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -5,39 +5,42 @@ import { ComputersCanvas } from "./canvas";
 
 const Hero = () => {
   return (
-    <section className={`relative w-full h-auto`}>
+    <section
+      className={`relative w-full min-h-screen mx-auto flex flex-col justify-center items-center px-4 sm:px-8`}
+    >
       <div
         className={`absolute inset-0 top-[120px]  max-w-7xl mx-auto ${styles.paddingX} flex flex-row items-start gap-5`}
       >
-        <div className="flex flex-col justify-center items-center mt-5">
+        <div className="flex flex-col justify-center items-center sm:items-start mt-5">
           <div className="w-5 h-5 rounded-full bg-[#915EFF]" />
           <div className="w-1 sm:h-80 h-40 violet-gradient" />
         </div>
 
         <div>
           <h1 className={`${styles.heroHeadText} text-white`}>
-            Hi, I&apos;m <span className="text-[#915eff]">Gideon.K</span>
+            Hi, I&apos;m <span className="text-[#915eff]">Kaiyian.G</span>
           </h1>
           <p className={`${styles.heroSubText} text-white-100 mt-2`}>
-            A Software Engineer... <br className="sm:block hidden" />Hacker ;)
+            A Software Engineer... <br className="sm:block hidden" />
+            Hacker ;)
           </p>
         </div>
       </div>
-      <div className="h-[100px] xs:h-[150px] md:h-[200px]" />
-      <ComputersCanvas />
-      <div className="h-[100px] xs:h-[150px] md:h-[200px]" />
-      <div className="absolute bottom-32 xs:bottom-10 w-full flex justify-center items-center">
+            {/* Spacer */}
+            <div className="my-10" />
+      {/* Canvas */}
+      <div className="w-full">
+        <ComputersCanvas />
+      </div>
+                  {/* Spacer */}
+                  <div className="my-10" />
+{/* Scroll indicator */}
+<div className="absolute bottom-10 w-full flex justify-center items-center">
         <a href="#about">
           <div className="w-[35px] h-[64px] rounded-3xl border-4 border-secondary flex justify-center items-start p-2">
             <motion.div
-              animate={{
-                y: [0, 24, 0],
-              }}
-              transition={{
-                duration: 1.5,
-                repeat: Infinity,
-                repeatType: "loop",
-              }}
+              animate={{ y: [0, 24, 0] }}
+              transition={{ duration: 1.5, repeat: Infinity, repeatType: "loop" }}
               className="w-3 h-3 rounded-full bg-secondary mb-1"
             />
           </div>
