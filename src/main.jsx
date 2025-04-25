50c5cd86de5590208a49f67bc04e47b2ee762c80
Giddy-K <gideonkipamet@gmail.com>
2025-04-25 04:41:01 +0300

    Wraped ther app in the Toast provider

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,28 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App.jsx";
+import "./index.css";
+import { Toaster } from "react-hot-toast";
 
-ReactDOM.createRoot(document.getElementById('root')).render(
+ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <App />
-  </React.StrictMode>,
-)
+    <Toaster
+      position="top-right"
+      toastOptions={{
+        duration: 7000, // 🕒 Toast will stay for 7 seconds
+        className:
+          "bg-[#1a1a1a] text-white font-medium border border-gray-700 shadow-lg rounded-lg p-4",
+        style: {
+          fontFamily: "Poppins, sans-serif",
+        },
+        success: {
+          icon: "👏",
+        },
+        error: {
+          icon: "⚠️",
+        },
+      }}
+    />
+  </React.StrictMode>
+);
